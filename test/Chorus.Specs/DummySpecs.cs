4d5acd6cf7051cb58763d72afd330c2166d12f6e
Zack Schrag <zackschrag@gmail.com>
2019-10-12 16:36:27 -0600

    Add license, solution, git ignore, and dummy test

---
+++ test/Chorus.Specs/DummySpecs.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Chorus.Specs
+{
+    public class DummySpecs
+    {
+        [Test]
+        public void should_pass()
+        {
+            Assert.Pass();
+        }
+    }
+}
