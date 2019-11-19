a0109a27c93d8232a5437cd3664b81718118b3d2
Zack Schrag <zackschrag@gmail.com>
2019-11-18 22:31:49 -0700

    Add strongly typed extensions for IDistributedLog

--- src/Chorus.DistributedLog.Extensions/StronglyTypedDistributedLogExtensions.cs
+++ src/Chorus.DistributedLog.Extensions/StronglyTypedDistributedLogExtensions.cs
@@ -5,13 +5,22 @@ using System.Threading.Tasks;
 
 namespace Chorus.DistributedLog.Extensions
 {
-    public static class GenericDistributedLogExtensions
+    public static class StronglyTypedDistributedLogExtensions
     {
         public static Task AppendAsync<T>(this IDistributedLog distributedLog, string streamName, T payload)
         {
             var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload));
 
             return distributedLog.AppendAsync(streamName, bytes);
+        }
+
+        public static async Task<T> RetrieveEntry<T>(this IDistributedLog distributedLog, string streamName, int offset)
+        {
+            var bytes = await distributedLog.RetrieveEntry(streamName, offset);
+
+            var payload = Encoding.UTF8.GetString(bytes);
+
+            return JsonConvert.DeserializeObject<T>(payload);
         }
     }
 }
---
+++ test/Chorus.Specs/Extensions/StronglyTypedDistributedLogExtensionsSpecs.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Chorus.DistributedLog.InMemory;
+using Chorus.DistributedLog.Extensions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Chorus.Specs.Extensions
+{
+    [TestFixture]
+    public class StronglyTypedDistributedLogExtensionsSpecs
+    {
+        [Test]
+        public async Task Should_be_able_to_append_strongly_typed_payload()
+        {
+            var inMemoryDistributedLog = new InMemoryDistributedLog();
+
+            await inMemoryDistributedLog.AppendAsync("my-stream", new TestEvent {TestString = "123"});
+
+            var testEvent = await inMemoryDistributedLog.RetrieveEntry<TestEvent>("my-stream", 0);
+
+            Assert.That(testEvent.TestString, Is.EqualTo("123"));
+        }
+
+        [Test]
+        public async Task Should_get_serialization_exception_on_mismatched_type()
+        {
+            var inMemoryDistributedLog = new InMemoryDistributedLog();
+
+            await inMemoryDistributedLog.AppendAsync("my-stream", new {Abc = "123"});
+
+            Assert.ThrowsAsync<JsonSerializationException>(() =>
+                inMemoryDistributedLog.RetrieveEntry<NonDeserializableType>("my-stream", 0));
+        }
+    }
+
+    public class TestEvent
+    {
+        public string TestString { get; set; }
+    }
+
+    public class NonDeserializableType
+    {
+        public string TestString { get; private set; }
+
+        private NonDeserializableType(string abc)
+        {
+        }
+    }
+}
